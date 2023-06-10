98fdf1edded9e09df09689fbd8a7e4b1227ab79e
Peter Strössler <7starch@gmail.com>
2023-06-10 19:30:34 +0200

    implemented currency exchange calulation

--- admin/components/Detail/Detail.styles.ts
+++ admin/components/Detail/Detail.styles.ts
@@ -4,6 +4,13 @@ const styles = StyleSheet.create({
   headline: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
   }
 });
 
